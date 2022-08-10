dc0b19d2af385817e416b5b32cf4b87cd4b2b70b
ChristianAlexander1993 <christian.alexandergit@gmail.com>
2022-08-09 19:01:10 -0700

    Done

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Select some elements...
+
+let header = document.querySelector('#page-header')
+header.style.textAlign = 'left'
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.transform = 'scaleY(-1)'
+}
+
+let dogName = document.querySelectorAll('.dog-name');
+for(i = 0; i < dogName.length; i++) {
+    // dogName[i].style.backgroundColor = 'red'
+    dogName[i].style.textAlign = 'left'
+}
+
+
+let foot = document.querySelector('.footer')
+foot.style.backgroundColor = 'black'
+foot.style.color = 'white'
